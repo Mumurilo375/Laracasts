98aa26d445052de57e9470627f6b685f8145e7c7
Mumurilo375 <murilinho.pequeno@gmail.com>
2026-02-07 20:14:20 -0300

    Conserta relações de chave primaria nas migrations

--- database/migrations/2026_01_26_004148_create_job_listings_table.php
+++ database/migrations/2026_01_26_004148_create_job_listings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
             //$table->unsignedBigInteger('employer_id');
-            $table->foreignIdFor(Employer::class);
+            $table->foreignIdFor(Employer::class)->constrained();
             $table->string('title');
             $table->string('salary');
             $table->timestamps();
--- database/migrations/2026_02_02_234631_create_project_uzer_table.php
+++ database/migrations/2026_02_02_234631_create_project_uzer_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('project_id')->constrained()->cascadeOnDelete();
             $table->foreignId('uzer_id')->constrained()->cascadeOnDelete();
+            $table->boolean('is_manager')->default(false);
             $table->timestamps();
         });
     }
